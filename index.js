6d93b4a07eb9ce3f045f89eb8fbc482a0ceb322f
DHRUVKUMAR RAJYAGURU <dvr20012004@gmail.com>
2024-11-09 06:08:30 +0000

    Completed challenge

--- index.js
+++ index.js
@@ -23,7 +23,41 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+//Modified up until..
+app.get('/api/:date?',(req,res)=>{
+  date=req.params.date;
+  if(!date){
+    date=Date.now();
+  }
+ // console.log('#0:'+Date.parse(date))
+  /*if(Date.parse(date)!="Invalid String"){//if input is unix
+  date2=new Date(new Number(date));
+  }
+  else{//if input is utc
+    console.log('#2');
+    tdate=date.toString();
 
+    date2=new Date(tdate.slice(0,5),tdate.slice(6,8),tdate.slice(9));
+  }*/
+ // console.log(date,",",date2)
+  dateUNIX=Date.parse(date);
+  if(isNaN(dateUNIX)){
+    dateUNIX=new Date(date*1);
+  }
+  date3=new Date(dateUNIX)
+  dateUTC=date3.toUTCString();
+//  console.log("unix:"+dateUNIX+",utc:"+dateUTC);
+ /* if(isNaN(dateUNIX)||isNaN(dateUTC)){
+    res.send({error:"Invalid Date"});
+  }*/
+ if(dateUTC=="Invalid Date"){
+  res.send({error:dateUTC});
+ }
+  else{
+    res.send({unix:new Number(dateUNIX),utc:dateUTC.toString()});
+  }
+})
+//.. until HERE.
 
 
 // Listen on port set in environment variable or default to 3000
